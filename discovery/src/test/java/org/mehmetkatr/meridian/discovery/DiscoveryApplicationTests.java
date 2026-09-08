639e6211884c0aaf6543ad20f38553ebae67c08f
MehmetKaTR <mehmet2003kaan@outlook.com>
2026-09-08 16:22:14 +0300

    chore: bootstrap Meridian monorepo with parent POM and Eureka discovery server

---
+++ discovery/src/test/java/org/mehmetkatr/meridian/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mehmetkatr.meridian.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
