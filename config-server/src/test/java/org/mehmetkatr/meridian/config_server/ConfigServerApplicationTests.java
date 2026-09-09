6483b0038f42337d48a2ad3e7705ece922d9c87d
MehmetKaTR <mehmet2003kaan@outlook.com>
2026-09-09 08:32:05 +0300

    feat: add config-server with native backend and Eureka registration

---
+++ config-server/src/test/java/org/mehmetkatr/meridian/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mehmetkatr.meridian.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
